3df5e066c2a447fe88bbce17d242eae38e544213
kcs-rohankolappa <114976964+kcs-rohankolappa@users.noreply.github.com>
2023-05-22 01:23:07 -0700

    Add files via upload

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Nice to meet you, {name}!");
+        }
+    }
+}
